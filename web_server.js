53c10bba08e6b5140395c4f1c5566c4c8c20d720
Sally Lehman <sxlehman@godaddy.com>
2015-09-03 18:23:31 -0700

    first commit

---
+++ web_server.js
@@ -0,0 +1,18 @@
+
+var http = require('http');
+
+
+function request_handler (req, res) {
+  var body = "Hey, Thanks for calling!";
+  var content_length = body.legngth;
+  res.writeHead(200, {
+           'Content-Type': "text/plain",
+           'Content-Length': content_length
+           });
+res.end(body);
+}
+
+var s = http.createServer(request_handler);
+
+s.listen(8080);
+
